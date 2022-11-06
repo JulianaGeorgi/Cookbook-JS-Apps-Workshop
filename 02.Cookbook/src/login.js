c62fac0c886c118af441d2705dc27d26d05518ac
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2022-11-06 09:49:46 +0000

    Initial commit

---
+++ 02.Cookbook/src/login.js
@@ -0,0 +1,3 @@
+async function onLogin(email, password){
+    const response = await fetch('http://localhost:3030/users/login');
+}
---
+++ 02.Cookbook/src/register.js
@@ -0,0 +1,34 @@
+document.querySelector('form').addEventListener('submit', onSubmit);
+
+async function onSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const { email, password, rePass } = Object.fromEntries(formData.entries());
+    try {
+
+        if(!email || !password){
+            throw new Error('All fields must be filled');
+        }
+
+        if(password !== rePass){
+            throw new Error('Passwords don\'t match!');
+        }
+
+        const response = await fetch('http://localhost:3030/users/register', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application\json' },
+            body: JSON.stringify({ email, password })
+        });
+        const data = await response.json();
+        
+        if (response.status == 200) {
+            sessionStorage.setItem('authToken', data.accessToken);
+            window.location = 'index.html';
+        } else {
+            throw new Error(data.message);
+        }
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
